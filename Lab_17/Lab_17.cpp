209de7cc0a31b364f8cc17d671c6c52d7418ce25
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2025-05-18 12:25:10 +0300

    18 lab

--- Lab_17/Lab_17.cpp
+++ Lab_17/Lab_17.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <algorithm>
 std::string surnames[5] = {"Johnson", "Smith", "Jackson", "Pitt", "Shevchenko"};
 class Student{
     std::string surname;
@@ -77,6 +81,23 @@ void quickSort(Student* arr, int start, int end){
     }
 } 
 
+int simpleSearch(std::string text, std::string pattern){
+    int textLength = text.length();
+    int patternLength = pattern.length();
+    for (int i = 0; i <= textLength - patternLength; i++){
+        int j;
+        for (j = 0; j < patternLength; j++){
+            if (text[i + j] != pattern[j]){
+                break;
+            }
+        }
+        if (j == patternLength){
+            return i;
+        }
+    }
+    return -1;
+}
+
 std::vector<int> studentsWithMax(Student* arr, int size){
    int max = arr[0].getAvgGrade();
    std::vector<int> numbers;
@@ -110,37 +131,168 @@ void binarySearch(Student* arr, int start, int end, int element) {
     std::cout << "Student with average grade " << element << " not found.\n";
 }
 
+std::vector<int> buildKMPTable(const std::string& pattern) {
+    std::vector<int> lps(pattern.length(), 0);
+    int len = 0;
+    for (int i = 1; i < pattern.length(); ) {
+        if (pattern[i] == pattern[len]) {
+            lps[i++] = ++len;
+        } else {
+            if (len != 0)
+                len = lps[len - 1];
+            else
+                lps[i++] = 0;
+        }
+    }
+    return lps;
+}
+
+int KMPSearch(const std::string& text, const std::string& pattern) {
+    std::vector<int> lps = buildKMPTable(pattern);
+    int i = 0, j = 0;
+    while (i < text.length()) {
+        if (pattern[j] == text[i]) {
+            i++; j++;
+        }
+        if (j == pattern.length()) {
+            return i - j;
+        } else if (i < text.length() && pattern[j] != text[i]) {
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    return -1;
+}
+
+std::vector<int> buildBadCharTable(const std::string& pattern) {
+    const int ALPHABET_SIZE = 256;
+    std::vector<int> badChar(ALPHABET_SIZE, -1);
+    for (int i = 0; i < pattern.length(); ++i)
+        badChar[(unsigned char)pattern[i]] = i;
+    return badChar;
+}
+
+int BoyerMooreSearch(const std::string& text, const std::string& pattern) {
+    std::vector<int> badChar = buildBadCharTable(pattern);
+    int m = pattern.length();
+    int n = text.length();
+
+    int shift = 0;
+    while (shift <= n - m) {
+        int j = m - 1;
+        while (j >= 0 && pattern[j] == text[shift + j]) {
+            j--;
+        }
+        if (j < 0) {
+            return shift;
+        } else {
+            shift += std::max(1, j - badChar[(unsigned char)text[shift + j]]);
+        }
+    }
+    return -1;
+}
 int main(){
     srand(time(0));
-    std::cout << "Enter number of students\n";
-    int number;
-    std::cin >> number;
-    Student* studentArr = new Student[number];
-    for (int i = 0; i < number; i++){
-        std::vector<int> gradesArr;
-        for (int j = 0; j < 3; j++){
-            gradesArr.push_back(rand() % 100);
+    std::cout << "Choose task(1-2)\n";
+    int task;
+    std::cin>>task;
+    if (task != 1 && task != 2){
+        std::cout << "Invalid task\n";
+        return 0;
+    }
+    switch (task){
+        case 1:{
+            std::cout << "Enter number of students\n";
+            int number;
+            std::cin >> number;
+            Student* studentArr = new Student[number];
+            for (int i = 0; i < number; i++){
+                std::vector<int> gradesArr;
+                for (int j = 0; j < 3; j++){
+                    gradesArr.push_back(rand() % 100);
+                }
+                std::string surname = surnames[rand() % 5];
+                studentArr[i] = Student(surname, gradesArr);
+            }
+            std::vector<int> studentsWithMaxRes = studentsWithMax(studentArr, number);
+            std::cout << "Array of students\n";
+            for (int i = 0; i < number; i++){
+                studentArr[i].printStudent();
+            }   
+            std::cout << "Number of students with maximum avg grade = " << studentsWithMaxRes.size() << " and numbers of students: \n";
+            for (auto element : studentsWithMaxRes){
+                std::cout << element << " ";
+            }
+            std::cout << "\n";
+            std::cout << "Sorted array for binary search\n";
+            quickSort(studentArr, 0, number - 1);
+            for (int i = 0; i < number; i++){
+                studentArr[i].printStudent();
+            }  
+            std::cout << "Enter avg grade to find with binary search\n";
+            int avg;
+            std::cin >> avg;
+            binarySearch(studentArr, 0, number - 1, avg);
+            delete[] studentArr;
+            break;  
         }
-        std::string surname = surnames[rand() % 5];
-        studentArr[i] = Student(surname, gradesArr);
+        case 2:{
+            std::ifstream fileToRead("file.txt");
+            if (!fileToRead.is_open()){
+                std::cout << "File not found\n";
+                return 0;
+            }
+            std::string text;
+            std::string finalLine;
+            while (std::getline(fileToRead, text)){
+                finalLine += text + "\n";
+            }
+            std::cout << "Enter pattern to find\n";
+            std::cin.ignore();
+            std::string pattern;
+            std::getline(std::cin, pattern);
+            std::cout << "Text from file:\n" << finalLine << "\n";
+            std::cout << "Pattern to find: " << pattern << "\n";
+            std::cout << "Choose search method(1 - simple, 2 - KMP, 3 - Boyer Moore)\n";
+            int searchMethod;
+            std::cin >> searchMethod;
+            switch (searchMethod){
+                case 1: {
+                    int index = simpleSearch(finalLine, pattern);
+                    if (index != -1){
+                        std::cout << "Pattern found at index " << index << "\n";
+                    } else {
+                        std::cout << "Pattern not found\n";
+                    }
+                
+                }
+                break;
+                case 2:{
+                    int index = KMPSearch(finalLine, pattern);
+                    if (index != -1){
+                        std::cout << "Pattern found at index " << index << "\n";
+                    } else {
+                        std::cout << "Pattern not found\n";
+                    }
+                }
+                break;
+                case 3:{
+                    int index = BoyerMooreSearch(finalLine, pattern);
+                    if (index != -1){
+                        std::cout << "Pattern found at index " << index << "\n";
+                    } else {
+                        std::cout << "Pattern not found\n";   
+                 } 
+                }
+                break;
+                default:
+                    std::cout << "Invalid search method\n";
+                    return 0;
+            break;
+        }
     }
-    std::vector<int> studentsWithMaxRes = studentsWithMax(studentArr, number);
-    std::cout << "Array of students\n";
-    for (int i = 0; i < number; i++){
-        studentArr[i].printStudent();
-    }   
-    std::cout << "Number of students with maximum avg grade = " << studentsWithMaxRes.size() << " and numbers of students: \n";
-    for (auto element : studentsWithMaxRes){
-        std::cout << element << " ";
-    }
-    std::cout << "\n";
-    std::cout << "Sorted array for binary search\n";
-    quickSort(studentArr, 0, number - 1);
-    for (int i = 0; i < number; i++){
-        studentArr[i].printStudent();
-    }  
-    std::cout << "Enter avg grade to find with binary search\n";
-    int avg;
-    std::cin >> avg;
-    binarySearch(studentArr, 0, number - 1, avg);
+}
 }
---
+++ Lab_18/Lab_18.cpp
@@ -0,0 +1,267 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <locale>
+#include <codecvt>
+#include <io.h>
+#include <fcntl.h>
+#include <map>
+#include <vector>
+#include <iomanip>
+#include <limits>
+#include <algorithm>
+
+const std::wstring ukrAlphabet = L"абвгґдеєжзиийклмнопрстуфхцчшщьюя";
+
+std::map<wchar_t, wchar_t> simpleEncodings = {
+    {L'о', L'а'}, {L'а', L'б'}, {L'е', L'в'}, {L'і', L'г'}, {L'н', L'ґ'},
+    {L'т', L'д'}, {L'с', L'е'}, {L'р', L'є'}, {L'л', L'ж'}, {L'в', L'з'},
+    {L'к', L'и'}, {L'м', L'й'}, {L'д', L'ї'}, {L'п', L'к'}, {L'у', L'л'},
+    {L'з', L'м'}, {L'я', L'ь'}, {L'и', L'о'}, {L'б', L'п'}, {L'г', L'р'},
+    {L'ї', L'с'}, {L'й', L'т'}, {L'х', L'у'}, {L'ж', L'ф'}, {L'є', L'х'},
+    {L'ч', L'ц'}, {L'ю', L'ч'}, {L'ш', L'ш'}, {L'ц', L'щ'}, {L'щ', L'ю'},
+    {L'ф', L'я'}, {L'ґ', L'н'}, {L'ь', L'ґ'}
+};
+
+std::map<wchar_t, double> ukrLetterFrequencies = {
+    {L'о', 9.28}, {L'а', 8.34}, {L'е', 7.89}, {L'і', 7.56}, {L'н', 6.95},
+    {L'т', 6.29}, {L'с', 5.62}, {L'р', 5.01}, {L'л', 4.70}, {L'в', 4.48},
+    {L'к', 3.95}, {L'м', 3.78}, {L'д', 3.61}, {L'п', 3.42}, {L'у', 2.90},
+    {L'з', 2.65}, {L'я', 2.47}, {L'и', 2.20}, {L'б', 1.85}, {L'г', 1.70},
+    {L'ї', 1.49}, {L'й', 1.35}, {L'х', 1.11}, {L'ж', 0.95}, {L'є', 0.82},
+    {L'ч', 0.77}, {L'ю', 0.64}, {L'ш', 0.59}, {L'ц', 0.52}, {L'щ', 0.43},
+    {L'ф', 0.22}, {L'ґ', 0.15}, {L'ь', 0.13}
+};
+
+std::map<wchar_t, double> getLetterFrequencies(const std::wstring& text) {
+    std::map<wchar_t, double> frequencies;
+    int totalLetters = 0;
+
+    for (wchar_t ch : text) {
+        if (ukrLetterFrequencies.find(ch) != ukrLetterFrequencies.end()) {
+            frequencies[ch]++;
+            totalLetters++;
+        }
+    }
+
+    for (auto& pair : frequencies) {
+        pair.second = (pair.second / totalLetters) * 100;
+    }
+
+    return frequencies;
+}
+
+std::wstring decrypt(const std::wstring& text, int shift) {
+    shift = (shift % 32 + 32) % 32;
+    std::wstring result;
+    for (wchar_t ch : text) {
+        if (ch >= L'а' && ch <= L'я') {
+            int index = (ch - L'а' - shift + 32) % 32;
+            result += L'а' + index;
+        } else if (ch >= L'А' && ch <= L'Я') {
+            int index = (ch - L'А' - shift + 32) % 32;
+            result += L'А' + index;
+        } else {
+            result += ch;
+        }
+    }
+    return result;
+}
+
+void decryptFrequencyAnalysis(const std::wstring& text) {
+    auto cipherFrequencies = getLetterFrequencies(text);
+
+    std::vector<std::pair<wchar_t, double>> cipherSorted(cipherFrequencies.begin(), cipherFrequencies.end());
+    std::vector<std::pair<wchar_t, double>> ukrSorted(ukrLetterFrequencies.begin(), ukrLetterFrequencies.end());
+
+    std::sort(cipherSorted.begin(), cipherSorted.end(), [](auto& a, auto& b) {
+        return a.second > b.second;
+    });
+
+    std::sort(ukrSorted.begin(), ukrSorted.end(), [](auto& a, auto& b) {
+        return a.second > b.second;
+    });
+
+    int userInput = 1, counter = 0;
+
+    while (userInput == 1 && counter < cipherSorted.size()) {
+        wchar_t cipherChar = cipherSorted[counter].first;
+
+        double minDiff = std::numeric_limits<double>::max();
+        wchar_t bestMatch = L' ';
+        for (auto& pair : ukrSorted) {
+            double diff = std::abs(pair.second - cipherSorted[counter].second);
+            if (diff < minDiff) {
+                minDiff = diff;
+                bestMatch = pair.first;
+            }
+        }
+
+        int shift = (cipherChar - bestMatch + 32) % 32;
+        std::wstring decrypted = decrypt(text, shift);
+
+        std::wcout << L"\nsymbols: '" << cipherChar << L"' → '" << bestMatch << L"' (зсув " << shift << L")\n";
+        std::wcout << L"Dexrypted text:\n" << decrypted << L"\n";
+        std::wcout << L"Continue? (1 - так, 0 - ні): ";
+        std::wcin >> userInput;
+
+        counter++;
+    }
+}
+
+std::wstring ceasar(const std::wstring& text, int shift) {
+    shift = (shift % 32 + 32) % 32;
+    std::wstring result;
+    for (wchar_t ch : text) {
+        if (ch >= L'а' && ch <= L'я') {
+            int index = (ch - L'а' + shift) % 32;
+            result += L'а' + index;
+        } else if (ch >= L'А' && ch <= L'Я') {
+            int index = (ch - L'А' + shift) % 32;
+            result += L'А' + index;
+        } else {
+            result += ch;
+        }
+    }
+    return result;
+}
+
+std::wstring enterText() {
+    std::wstring text;
+    std::wcout << L"Enter text: ";
+    std::wcin.ignore();
+    std::getline(std::wcin, text);
+    return text;
+}
+
+std::wstring readFile(const std::string& filename) {
+    std::wifstream file(filename);
+    file.imbue(std::locale(file.getloc(), new std::codecvt_utf8<wchar_t>));
+    if (!file) {
+        std::wcerr << L"Failed to open file.\n";
+        return L"";
+    }
+    std::wstringstream buffer;
+    std::wstring line;
+    while (std::getline(file, line)) {
+        buffer << line << L'\n';
+    }
+    return buffer.str();
+}
+
+std::wstring simpleEncoding(const std::wstring& text) {
+    std::wstring result;
+    for (wchar_t ch : text) {
+        if (simpleEncodings.find(ch) != simpleEncodings.end()) {
+            result += simpleEncodings[ch];
+        } else {
+            result += ch;
+        }
+    }
+    return result;
+}
+
+std::wstring simpleDecoding(const std::wstring& text) {
+    std::wstring result;
+    for (wchar_t ch : text) {
+        auto it = std::find_if(simpleEncodings.begin(), simpleEncodings.end(),
+                               [ch](const std::pair<wchar_t, wchar_t>& pair) { return pair.second == ch; });
+        if (it != simpleEncodings.end()) {
+            result += it->first;
+        } else {
+            result += ch;
+        }
+    }
+    return result;
+}
+int main() {
+    setlocale(LC_ALL, "");
+    _setmode(_fileno(stdout), _O_U8TEXT);
+    _setmode(_fileno(stdin), _O_U8TEXT);
+
+    while (true) {
+        std::wcout << L"\nChoose a task (1-6, 0 for exit)): ";
+        int task;
+        std::wcin >> task;
+        if (task == 0) break;
+        switch (task) {
+            case 1:{
+                std::wcout << L"Select input method (1 - console, 2 - file): ";
+                int choice;
+                std::wcin >> choice;
+                std::wstring text;
+                if (choice == 1) {
+                    text = enterText();
+                } else if (choice == 2) {
+                    text = readFile("text.txt");
+                } else {
+                    std::wcerr << L"Invalid input method.\n";
+                    continue;
+                }
+
+                std::wcout << L"Enter shift value (positive or negative): ";
+                int shift;
+                std::wcin >> shift;
+
+                std::wstring encrypted = ceasar(text, shift);
+                std::wstring decrypted = decrypt(encrypted, shift);
+
+                std::wcout << L"\nEncrypted text: " << encrypted;
+                std::wcout << L"\nDecrypted text: " << decrypted << L"\n";
+                std::wcout << L"\nFrequency analysis:\n";
+                decryptFrequencyAnalysis(encrypted);
+            }
+                break;
+            case 2:
+                {
+                     std::wcout << L"Select input method (1 - console, 2 - file): ";
+                    int choice;
+                    std::wcin >> choice;
+                    std::wstring text;
+                    if (choice == 1) {
+                        text = enterText();
+                    } else if (choice == 2) {
+                        text = readFile("text.txt");
+                    } else {
+                        std::wcerr << L"Invalid input method.\n";
+                        continue;
+                    }
+                    std::wcout << L"Simple encoding:\n";
+                    std::wcout << L"Original text: " << text << L"\n";
+                    std::wstring encoded = simpleEncoding(text);
+                    std::wcout << L"Encoded text: " << encoded << L"\n";
+                    std::wcout << L"Decoded text: " << simpleDecoding(encoded) << L"\n";
+                }
+                break;
+            case 3:
+                {
+                    std::wcout << L"Select input method (1 - console, 2 - file): ";
+                    int choice;
+                    std::wcin >> choice;
+                    std::wstring text;
+                    if (choice == 1) {
+                        text = enterText();
+                    } else if (choice == 2) {
+                        text = readFile("text.txt");
+                    } else {
+                        std::wcerr << L"Invalid input method.\n";
+                        continue;
+                    }
+                }
+                break;
+            case 4:
+                std::wcout << L"Task 4 selected.\n";
+                break;
+            case 5:
+                std::wcout << L"Task 5 selected.\n";
+                break;
+            case 6:
+                std::wcout << L"Task 6 selected.\n";
+                break;
+            default:
+                std::wcerr << L"Invalid task number.\n";
+        }
+    }
+    return 0;
+}
